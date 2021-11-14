a39c2da83a12efbc66372ac5de642c76104e9e6c
woroodZoabi <92725568+woroodZoabi@users.noreply.github.com>
2021-11-14 20:35:48 +0200

    Questions Managment Frames

---
+++ src/View/QuestionsManagmentController.java
@@ -0,0 +1,20 @@
+package View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class QuestionsManagmentController implements Initializable {
+
+	public QuestionsManagmentController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/View/ViewLogic.java
+++ src/View/ViewLogic.java
@@ -1,5 +1,15 @@
 package View;
+import java.io.IOException;
+import java.net.URL;
+
+import Controller.SysData;
+
+import javafx.application.Platform;
+import javafx.stage.StageStyle;
+
+
 
 public class ViewLogic {
-
+	protected static SysData sysdata = SysData.getInstance();
+	
 }
