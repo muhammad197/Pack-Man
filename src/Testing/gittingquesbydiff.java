0752ce5a8e7b53bda7443c75b2526841494a697e
muhammad197 <muhammad_m_197@hotmail.com>
2021-12-20 17:00:11 +0200

    test added, how to play changed

---
+++ src/Testing/gittingquesbydiff.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import Controller.SysData;
+import Model.Question;
+import Utils.Level;
+
+class gittingquesbydiff {
+
+	@Test
+	void test() {
+		SysData instance = SysData.getInstance();
+		instance.getQuestions();
+		Question hardQtest = instance.randomQuestion(Level.hard);
+		Question medQtest = instance.randomQuestion(Level.medium);
+		Question easyQtest = instance.randomQuestion(Level.easy);
+		assertNotNull(hardQtest);
+		assertNotNull(medQtest);
+		assertNotNull(easyQtest);
+	}
+
+}
